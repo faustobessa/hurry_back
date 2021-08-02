9b2b8e7aece2b0c55970fca4fad35442341df72a
RaaschDev <devraasch@gmail.com>
2021-08-02 15:43:25 -0300

    pag

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    '', url(r'^retorno/pagseguro/', include('pagseguro.urls')),
+    path('retorno/pagseguro/', include('pagseguro.urls'))
 ]
 
